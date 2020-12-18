68b7447559ba2ae1221e8d386876bbc5bb39c680
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2020-12-18 01:15:56 +0000

    correct examples to match article

--- python-api-examples/python_api_examples/request.py
+++ python-api-examples/python_api_examples/request.py
@@ -22,6 +22,7 @@ def get_id_key():
 
 
 @retry(
+    retry=retry_if_exception_type(requests.exceptions.RequestException),
     wait=wait_exponential(max=60, min=1),
     stop=stop_after_attempt(3),
 )
